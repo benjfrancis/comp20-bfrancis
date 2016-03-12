083aeda5e788d5ee36eb0eab53285e055404f03b
Ben Francis <benfrancis@me.com>
2016-03-11 23:06:32 -0500

    added readme and attempted to implement the parse and xmlhttp request stuff

---
+++ messages/README.md
@@ -0,0 +1,9 @@
+## Ben Francis
+## Lab 6: Messages
+
+The lab has not been correctly implemented.  There are javascript errors, and 
+the messages do not display.
+
+I did not collaborate with anyone on this assignment.
+
+I spent an hour on this assignment.
--- messages/lab.js
+++ messages/lab.js
@@ -1,9 +1,31 @@
 
 
-request = new XMLHttpRequest();
 
-request.open("GET", "data.json", true);
 
-request.onreadystatechage = parse;
+function parse() 
+{
+        
+        request = new XMLHttpRequest();
 
-request.send(NULL);
+        request.open("GET", "messages.json", true);
+
+        request.onreadystatechange = parse;
+
+        request.send(NULL);
+
+        if (request.readyState == 4 && request.status == 200) {
+                result = "";
+                raw = request.responseText;
+                theMessages = JSON.parse(raw);
+                elem = document.getElementById("messages");
+                
+                for(i = 0; i < theMessages.length; i++) {
+                      result += theMessages[i]['id'] + " " + 
+                                theMessages[i]['content'] " " + 
+                                theMessages[i]['username'] + "<br>"; 
+                }
+
+                elem.innerHTML = result;
+
+
+}
